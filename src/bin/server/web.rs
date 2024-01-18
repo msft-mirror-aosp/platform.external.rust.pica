483be13e25e73bec97dfe97ce3ab3b5ef4d1a2d6
Carlos Galo <carlosgalo@google.com>
2024-01-18 23:03:40 +0000

    Revert "Merge remote-tracking branch 'aosp/upstream-main'"
    This reverts commit b56f333f65f80f5282ce79fe561bb2e334abf81e.
    
    Reason for revert: b/320853087. Will be running through ABTD for confirmation and before submitting the revert.
    
    Change-Id: I96dba0d5ffb3e62e785d27102720fab40b1842fe

--- src/bin/server/web.rs
+++ src/bin/server/web.rs
@@ -120,7 +120,7 @@ fn event_name(event: &PicaEvent) -> &'static str {
 async fn handle(
     mut req: Request<Body>,
     tx: mpsc::Sender<PicaCommand>,
-    events: broadcast::Receiver<PicaEvent>,
+    events: broadcast::Sender<PicaEvent>,
 ) -> Result<Response<Body>, Infallible> {
     let static_file = STATIC_FILES
         .iter()
@@ -168,7 +168,7 @@ async fn handle(
         .collect::<Vec<_>>()[..]
     {
         ["events"] => {
-            let stream = BroadcastStream::new(events).map(|result| {
+            let stream = BroadcastStream::new(events.subscribe()).map(|result| {
                 result.map(|event| {
                     format!(
                         "event: {}\ndata: {}\n\n",
@@ -256,7 +256,7 @@ pub async fn serve(
         let events = events.clone();
         async move {
             Ok::<_, Infallible>(service_fn(move |req| {
-                handle(req, tx.clone(), events.subscribe())
+                handle(req, tx.clone(), events.clone())
             }))
         }
     });
